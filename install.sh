51b2a86c0ee212c9cbd77e5ca38f4d7529a9dcac
Kushuh <kuzanisu@gmail.com>
2020-07-28 14:06:19 +0200

    added installer

--- install.sh
+++ install.sh
@@ -2,12 +2,18 @@ DIR=$(pwd)
 
 cd ~ || (printf "\033\0;31mUnable to open root folder : please make sure you run this script with admin right.\033[0m\nrun : sudo sh install.sh\n" && exit)
 
-printf "\033[0;36mInstalling couchbase-setup...\033[0m\n"
-printf "Copying files...\n"
+printf "\033[0;36mInstalling couchbase-jc...\033[0m\n"
+printf "Copying files..."
 
-mkdir ~/bin
-cd ~/bin || (printf "\033[0;31mCouldn't move to bin folder. Exiting installer...\033[0m\n" && exit)
+mkdir ~/bin/couchbase-jc
+cd ~/bin/couchbase-jc || (printf "\033[0;31mCouldn't move to bin folder. Exiting installer...\033[0m\n" && exit)
 
-git clone https://github.com/a-novel/couchbase-setup couchbase-setup
+curl https://github.com/a-novel/couchbase-setup/blob/master/meta.json --output meta.json
+curl https://github.com/a-novel/couchbase-setup/blob/master/setup-couchbase.sh --output setup-couchbase.sh
+
+touch .bash_profile
+echo "PATH=~/bin:$PATH" > ".bash_profile"
+echo "alias couchbase-jc='couchbase-setup.sh'" >> ".bash_profile"
 
 cd "$DIR" || exit
+printf "\r\033[K\o3[0;33mSetup complete !\033[0m\nYou can now use couchbase-jc in your local terminal."
